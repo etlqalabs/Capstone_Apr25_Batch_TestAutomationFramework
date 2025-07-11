3b3cda07f56e0fa74cbe87e9808a45d5096abdd3
Hetu <sahu.hetram@gmail.com>
2025-07-11 08:39:35 +0530

    all tests done

--- TestScripts/test_Data_Quality.py
+++ TestScripts/test_Data_Quality.py
@@ -15,6 +15,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+@pytest.mark.skip
 @pytest.mark.smoke
 @pytest.mark.regression
 def test_DataQuality_supplier_data_file_availability():
@@ -25,3 +26,19 @@ def test_DataQuality_supplier_data_file_availability():
         logger.error(f"Error while checking the file existance {e}")
         pytest.fail("Test case execution fil availability check  has failed")
     logger.info(f"Test case execution fil availability check  has completed....")
+
+
+## Test case to check the refertial integrrity
+
+def test_referentialIntegrity_src_staging_tgt_mysql(connect_to_mysql_database_staging,connect_to_mysql_database_target):
+    query_expected = """select store_id from staging_stores order by store_id"""
+    df_expected = pd.read_sql(query_expected,connect_to_mysql_database_staging)
+    query_actual = """select store_id from fact_sales order by store_id"""
+    df_actual= pd.read_sql(query_actual, connect_to_mysql_database_target)
+
+    df_all_matched = df_actual[df_actual['store_id'].isin(df_expected['store_id'])]
+    df_not_macthed = df_actual[~df_actual['store_id'].isin(df_expected['store_id'])]
+    print(df_not_macthed)
+    df_not_macthed.to_csv("LogFiles/notmatchingdata.csv",index=False)
+    assert df_not_macthed.empty,"There is extra store_id in target vs source/stag"
+
